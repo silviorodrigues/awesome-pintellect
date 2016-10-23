bf683fada2170d2bf8e8782e2ba464e213c0e642
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-10-23 12:33:53 -0200

    Angular routes

---
+++ assets/js/routes.js
@@ -0,0 +1,10 @@
+angular.module('app', ['ngRoute']);
+
+angular.module('app').config(function($routeProvider) {
+  $routeProvider
+    .when('/', {templateUrl:'feed.html'})
+    .when('/single-post', {templateUrl:'single-post.html'})
+    .when('/show-profile', {templateUrl:'show-profile.html'})
+    .when('/edit-profile', {templateUrl:'edit-profile.html'})
+    .otherwise({redirectTo:'/'});
+});
