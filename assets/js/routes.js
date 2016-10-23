35d29e51f8804103157980d47de5e60b8d2b7229
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-10-23 16:59:55 -0200

    Added faq route and fixed search icon

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -6,5 +6,6 @@ angular.module('app').config(function($routeProvider) {
     .when('/single-post', {templateUrl:'single-post.html'})
     .when('/show-profile', {templateUrl:'show-profile.html'})
     .when('/edit-profile', {templateUrl:'edit-profile.html'})
+    .when('/faq', {templateUrl:'faq.html'})
     .otherwise({redirectTo:'/'});
 });
